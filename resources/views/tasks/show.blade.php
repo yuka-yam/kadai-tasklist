d0a6989844619700d5244279d0d661b1849fadcc
yuka.yamamoto <y.mmm011@gmail.com>
2023-06-20 03:42:07 +0000

    validation , add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -12,7 +12,12 @@
             <th>id</th>
             <td>{{ $task->id }}</td>
         </tr>
-
+        
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        
         <tr>
             <th>メッセージ</th>
             <td>{{ $task->content }}</td>
